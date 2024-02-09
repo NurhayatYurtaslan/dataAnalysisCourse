e9bda455f84266ffe67dbb58a89455bbe8b62616
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-02-09 10:48:09 +0300

    Create create_array.py

---
+++ Chapter_02/create_array.py
@@ -0,0 +1,7 @@
+import numpy as np
+#myar=np.array([12,3,44,123,53,64])
+#print(myar[-1])
+mystr=np.array(['fdfd','rere','retr','fdgg'],dtype=np.str_)
+print(mystr[-1])
+print(mystr[3])
+print(mystr[-2])
