5d5271ad671d83a3ae04929067bc343c17e25718
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-02-14 12:04:15 +0300

    Update dataType.py

--- Chapter_02/dataType.py
+++ Chapter_02/dataType.py
@@ -1,3 +1,25 @@
 import numpy as np
-ar=np.array([3,4,5,6,7,8], dtype=np.int32)
+
+ar = np.array([3, 4, 5, 6, 7, 8], dtype=np.int32)
 print(ar)
+print(ar.dtype)
+
+ar = np.array([3, 4, 5, 6, 7, 8], dtype='f')
+print(ar)
+print(ar.dtype)
+
+ar = np.array([True, False, False, True], dtype=np.bool_)
+print(ar)
+print(ar.dtype)
+
+ar = np.array(['kjdsjdskj', 'dsdsd', 'dsdsew'], dtype=np.str_)
+print(ar)
+print(ar.dtype)
+
+ar = np.array(['k', 'd', 'w'], dtype='S1')
+print(ar)
+print(ar.dtype)
+
+ar=np.array([1+3j,3+6j,6+3j],dtype=np.complex64)
+print(ar)
+print(ar.dtype)
