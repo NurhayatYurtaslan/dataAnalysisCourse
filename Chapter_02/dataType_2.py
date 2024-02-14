084c6e75e640dfb0759505c44e182f2d5e2a229d
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-02-14 12:14:55 +0300

    Create dataType_2.py

---
+++ Chapter_02/dataType_2.py
@@ -0,0 +1,11 @@
+d1=np.datetime64('2024-02-14')
+d2=np.datetime64('1999-12-02')
+diff=d1-d2
+print(diff)
+print(type(diff))
+print(diff.dtype)
+
+"""print(type(diff)) ifadesinin çıktısı, diff değişkeninin türünü gösterir. Bu durumda, diff değişkeni bir numpy.timedelta64 nesnesidir. numpy.timedelta64, iki tarih veya zaman damgası arasındaki farkı temsil etmek için kullanılan özel bir veri tipidir.
+
+print(diff.dtype) ifadesinin çıktısı ise diff değişkeninin veri tipini gösterir. Bu durumda, diff değişkeninin veri tipi timedelta64[D] şeklindedir. [D] ifadesi, farkın gün cinsinden temsil edildiğini gösterir. timedelta64 veri tipi, zaman farklarını toplama, çıkarma ve karşılaştırma gibi çeşitli işlemleri gerçekleştirmenizi sağlar."""
+
