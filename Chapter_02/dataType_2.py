7e77cf8f42627a38f1c4409a3d2e3460cfacc567
NurhayatYurtaslan <80510115+NurhayatYurtaslan@users.noreply.github.com>
2024-02-14 13:49:34 +0300

    Update dataType_2.py

--- Chapter_02/dataType_2.py
+++ Chapter_02/dataType_2.py
@@ -9,3 +9,10 @@ print(diff.dtype)
 
 print(diff.dtype) ifadesinin çıktısı ise diff değişkeninin veri tipini gösterir. Bu durumda, diff değişkeninin veri tipi timedelta64[D] şeklindedir. [D] ifadesi, farkın gün cinsinden temsil edildiğini gösterir. timedelta64 veri tipi, zaman farklarını toplama, çıkarma ve karşılaştırma gibi çeşitli işlemleri gerçekleştirmenizi sağlar."""
 
+dt=np.datetime64('2023-07-06 12:30:00')
+print(type(dt))
+
+mix_ar=np.array([1,3,4,False,[1,2,3],'jafri'],dtype=object)
+print(mix_ar)
+print(type(mix_ar))
+print(mix_ar.dtype)
